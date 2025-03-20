91e844d64dc284a3d3879223d0b601244edb92fd
prakhar singh <prakharsingh98765432@gmail.com>
2025-03-20 12:40:40 +0530

    End project

--- index.js
+++ index.js
@@ -22,3 +22,20 @@ function openmenu(){
 function closemenu(){
     sidemenu.style.right="-200px";
 }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw119T_N78LBa8w8TbqWWAYsZDytzgCJp2dFTf2FaV0QWeffkVtzM91ZrlFHyk9l60/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg=document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response =>{
+        msg.innerHTML="Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },1000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
