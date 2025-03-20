880f7f56f857c5429bad1b3e833cce47493cd320
prakhar singh <prakharsingh98765432@gmail.com>
2025-03-20 13:19:34 +0530

    done

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbw119T_N78LBa8w8TbqWW
         msg.innerHTML="Message sent successfully"
         setTimeout(function(){
             msg.innerHTML = ""
-        },1000)
+        },500)
         form.reset()
       })
       .catch(error => console.error('Error!', error.message))
