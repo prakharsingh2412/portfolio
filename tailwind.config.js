569c0a3bc7a619dd220731583dc37ec15c210efd
Prakhar Singh <prakharsingh98765432@gmail.com>
2025-10-06 13:50:46 +0530

    final change

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E40AF',
+        secondary: '#FACC15'
+      },
+    },
+  },
+  plugins: [],
+}
